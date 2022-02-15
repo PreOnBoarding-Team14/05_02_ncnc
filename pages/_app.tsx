0234454e8d9aa612bb389b6d20f7b6ead7540053
ydh94 <dbeogud9411@gmail.com>
2022-02-16 06:10:32 +0900

    Modify - css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from 'next/app';
 import Layout from 'components/Layout';
 import '../styles/globals.css';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    document.getElementById('__next').className = 'background';
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
